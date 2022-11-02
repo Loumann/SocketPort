1019be1d42a3ab9d63f19a6e8c25b38b3189ba0c
Loumann <Bleyka80@gmail.com>
2022-11-02 16:07:02 +0500

    Надо будет попольше разобраться с регулярными выражениями. Прибор програ видет, данные отсылает, все норм пока.

--- PortScanner/Program.cs
+++ PortScanner/Program.cs
@@ -1,23 +1,103 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.IO.Ports;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
 
-string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
-var data = new string[]
+namespace COMDataExchanger
 {
-    "tom@gmail.com",
-    "+12345678999",
-    "bob@yahoo.com",
-    "+13435465566",
-    "sam@yandex.ru",
-    "+43743989393"
-};
-
-Console.WriteLine("Email List");
-for (int i = 0; i < data.Length; i++)
-{
-    if (Regex.IsMatch(data[i], pattern, RegexOptions.IgnoreCase))
+    class Program
     {
-        Console.WriteLine(data[i]);
+        static SerialPort _serialPort = null;
+        static bool session;
+
+        static void Main(string[] args)
+        {
+            AllPorts();
+
+            try
+            {
+                _serialPort = new SerialPort
+                {
+                    PortName = Console.ReadLine() ?? throw new InvalidOperationException()
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            if (_serialPort == null)
+            {
+                Console.WriteLine("The text format is not correct.");
+                return;
+            }
+            _serialPort.ReadTimeout = -1;
+            _serialPort.WriteTimeout = 2000;
+            _serialPort.BaudRate = 19200;
+
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(_serialPort_DataRecieved);
+
+            try
+            {
+                _serialPort.Open();
+                session = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Write();
+
+            //try
+            //{
+            //    _serialPort.Close();
+            //    session = false;
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+            Console.ReadKey();
+        }
+
+        private static void AllPorts()
+        {
+            var ports = SerialPort.GetPortNames();
+
+            for (int i = 0; i < ports.Length; i++)
+            {
+                Console.WriteLine("[" + i.ToString() + "] " + ports[i].ToString());
+            }
+        }
+        
+        private static void Write()
+        {
+            while (session == true)
+            {
+                var message = Console.ReadLine();
+
+                _serialPort.WriteLine($"<{_serialPort.PortName}>: {message}");
+            }
+        }
+
+
+        // Доделать еще надо будет регулярные выражения.
+
+        private static void _serialPort_DataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            var recievedData = _serialPort.ReadExisting();
+            Console.WriteLine(recievedData);
+            Regex regex = new Regex(@"BLD(\w*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            MatchCollection match = regex.Matches(recievedData);
+            if (match.Count > 0)
+            {
+                
+            }
+        }
     }
 }
