af4c9f9d3cd703753f0603c9cf09312872b7d09c
Loumann <Bleyka80@gmail.com>
2022-11-10 17:28:49 +0500

    Жапан

--- PortScanner/JsonClass.cs
+++ PortScanner/JsonClass.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +9,37 @@ using System.Threading.Tasks.Dataflow;
 
 namespace PortScanner
 {
-    internal class JsonClass
+    public struct JsonObject
     {
-        public string Name { get; set; }
-        public int Chars { get; set; }
+        
+        [JsonProperty("BLD")]
+        public string BLD { get; set; }
 
+        [JsonProperty("UBG")]
+        public string UBG { get; set; }
 
+        [JsonProperty("BIL")]
+        public string BIL { get; set; }
+
+        [JsonProperty("PRO")]
+        public string PRO { get; set; }
+
+        [JsonProperty("NIT")]
+        public string NIT { get; set; }
+
+        [JsonProperty("KET")]
+        public string KET { get; set; }
+
+        [JsonProperty("GLU")]
+        public string GLU { get; set; }
+
+        [JsonProperty("pH")]
+        public string PH { get; set; }
+
+        [JsonProperty("SG")]
+        public string SG { get; set; }
+
+        [JsonProperty("LEU")]
+        public string LEU { get; set; }
     }
 }
--- PortScanner/Program.cs
+++ PortScanner/Program.cs
@@ -1,10 +1,15 @@
-﻿using PortScanner;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PortScanner;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 
 
@@ -12,13 +17,11 @@ namespace COMDataExchanger
 {
     class Program
     {
-
         public static StringBuilder sb = new StringBuilder();
 
         static SerialPort _serialPort = null;
         static bool session;
-
-
+        static int[] jsonData;
 
 
         static void Main(string[] args)
@@ -44,7 +47,7 @@ namespace COMDataExchanger
                 Console.WriteLine("The text format is not correct.");
                 return;
             }
-            _serialPort.ReadTimeout = -1;
+            _serialPort.ReadTimeout = 2000;
             _serialPort.WriteTimeout = 2000;
             _serialPort.BaudRate = 19200;
 
@@ -59,20 +62,7 @@ namespace COMDataExchanger
             {
                 Console.WriteLine(ex.Message);
             }
-
             Write();
-
-            //try
-            //{
-            //    _serialPort.Close();
-            //    session = false;
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex.Message);
-            //}
-
-
             Console.ReadKey();
         }
 
@@ -96,34 +86,79 @@ namespace COMDataExchanger
             }
         }
 
-
-       
-
-
-
-
-
-
-        //регулярное выражение и вывод с записью в json объект
         private static void _serialPort_DataRecieved(object sender, SerialDataReceivedEventArgs e)
         {
+            var selectedPeople = new List<string>();
             var recievedData = _serialPort.ReadExisting();
             Console.WriteLine(recievedData);
-            string json = JsonSerializer.Serialize(recievedData);
-            Console.WriteLine(json.Length);
 
-            Regex regex = new Regex(@"^\* \w\w\w", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+
+            Regex regex = new Regex(@"^\* \w\w\w\s\s\W\W...........", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
             MatchCollection matches = regex.Matches(recievedData);
+           
+            Thread.Sleep(500);
+
             if (matches.Count > 0)
             {
                 foreach (Match match in matches)
                     Console.WriteLine(match.Value);
-                Console.WriteLine("Найдено");
-
+                    Console.WriteLine("Найдено");
             }
             else
             {
                 Console.WriteLine("Нету");
+            }
+            JObject o = JObject.Parse(matches.ToString());
+
+            try
+            {
+                JsonObject jsonObject = new JsonObject()
+                {
+                    BLD = jsonData[0].ToString(),
+                    UBG = jsonData[1].ToString(),
+                    BIL = jsonData[2].ToString(),
+                    PRO = jsonData[3].ToString(),
+                    NIT = jsonData[4].ToString(),
+                    KET = jsonData[5].ToString(),
+                    GLU = jsonData[6].ToString(),
+                    PH = jsonData[7].ToString(),
+                    SG = jsonData[8].ToString(),
+                    LEU = jsonData[9].ToString()
+                };
+                var jsonString = JsonConvert.SerializeObject(jsonObject);
+                Console.WriteLine(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+         static void JsonCon(string[] jsonData)
+        { 
+            try
+            {
+                JsonObject jsonObject = new JsonObject()
+                {
+                    BLD = jsonData[0],
+                    UBG = jsonData[1],
+                    BIL = jsonData[2],
+                    PRO = jsonData[3],
+                    NIT = jsonData[4],
+                    KET = jsonData[5],
+                    GLU = jsonData[6],
+                    PH = jsonData[7],
+                    SG = jsonData[8],
+                    LEU = jsonData[9]
+                };
+                var jsonString = JsonConvert.SerializeObject(jsonObject);
+                Console.WriteLine(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
